a8e54f26794be137e553147622758416379a451e
BaZz <bazuaye50@gmail.com>
2023-09-20 19:11:54 +0100

    login error

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import style from "./style.module.scss";
+import { useForm } from "react-hook-form";
 
 export function LoginPage() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
 	const ErroHandler = ({ children }: { children: React.ReactNode }) => (
 		<div className={style.holders}>{children}</div>
 	);
@@ -9,7 +16,10 @@ export function LoginPage() {
 		<div className={style.container}>
 			<div className={style.side}></div>
 			<div className={style.main}>
-				<form>
+				<form
+					onSubmit={handleSubmit((data) => {
+						console.log(data, errors);
+					})}>
 					<h1>Login</h1>
 					<p className={style.text}>
 						Only student of the force can have access
@@ -17,17 +27,32 @@ export function LoginPage() {
 					<div className={style.inputs}>
 						<ErroHandler>
 							<input
-								required
+								{...register("email", {
+									required: {
+										message: "Email is required",
+										value: true,
+									},
+									value: "user@example.com",
+								})}
 								type="email"
 								placeholder="Email"
 							/>
+							<p>{errors.email?.message?.toString()}</p>
 						</ErroHandler>
 						<ErroHandler>
 							<input
+								{...register("password", {
+									required: {
+										message: "Password is required",
+										value: true,
+									},
+									value: "1Password",
+								})}
 								required
 								type="password"
 								placeholder="Password"
 							/>
+							<p>{errors.password?.message?.toString()}</p>
 						</ErroHandler>
 					</div>
 					<div className={style.btn}>
